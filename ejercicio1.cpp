6eff09aba690acdd72fd007d97ebdbe84649cbcb
vmanueltole1 <37409075+vmanueltole1@users.noreply.github.com>
2018-04-01 02:56:14 -0500

    Update and rename holamundo.cpp to ejercicio1.cpp

---
+++ ejercicio1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int n;
+  cout<<"ingresar edad :";
+  cin>>n;
+  if(n>=18)
+  {
+    cout<<"es mayor edad ";
+  }
+  else
+  {
+    cout<<"es manor edad ";
+  }
+}
--- holamundo.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(int argc, char *argv[]) {
-	cout<<"hola"<<endl;
-	cout<<"victor toledo gamarra "<<endl;
-	return 0;
-}
-
